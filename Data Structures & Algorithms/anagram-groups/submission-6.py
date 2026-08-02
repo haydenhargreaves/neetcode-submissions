cbea367994974d3f4408fdf7c5b67e68b02e4332
Hayden Hargreaves <104033825+haydenhargreaves@users.noreply.github.com>
2026-08-02 16:54:49 -0500

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = {}
+
+        for word in strs:
+            chars = [0] * 26
+            for char in word:
+                i = ord(char)-97
+                chars[i] += 1
+            key = ','.join([str(char) for char in chars])
+            hash[key] = hash.get(key, []) + [word]
+
+        return list(hash.values())
