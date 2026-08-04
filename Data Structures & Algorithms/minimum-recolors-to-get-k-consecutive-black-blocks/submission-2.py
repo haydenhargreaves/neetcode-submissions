4a6ac0161193c8901ed922aef39ec3bac8e690e7
Hayden Hargreaves <104033825+haydenhargreaves@users.noreply.github.com>
2026-08-04 14:55:51 -0500

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-2

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        l, r, maxCount = 0, k, 0
+        
+        while r <= len(blocks):
+            cur = len([char for char in blocks[l:r] if char == 'B'])
+            maxCount = max(maxCount, cur)
+            l, r, = l+1, r+1
+
+        return k - maxCount
+        
