af4e5c81d09f0cf2421a77bf479a47d213ed1567
Hayden Hargreaves <104033825+haydenhargreaves@users.noreply.github.com>
2026-09-18 14:13:44 -0700

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.cs
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public int ScoreOfString(string s) 
+    {
+        int acc = 0;
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            acc += Math.Abs((int)s[i] - (int)s[i+1]);
+        }
+
+        return acc;
+    }
+}
